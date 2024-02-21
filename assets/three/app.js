bf309ad31482c62f4bad363a185fda07ee3c3bb0
Thibault Delgrande <thib@ultdelgran.de>
2024-02-21 15:25:11 +0100

    Atomisation du code

--- assets/three/app.js
+++ assets/three/app.js
@@ -1,19 +1,9 @@
-import * as THREE from 'three';
-import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
 import './app.css';
-import GUI from 'lil-gui';
+import * as THREE from 'three';
 import PlayerControls from './global/controles';
 import Text3D from './global/text';
-import { FontLoader } from 'three/addons/loaders/FontLoader.js';
-import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import GUI from 'lil-gui';
 
-
-
-/**
- * GUI
- */
-
-const gui = new GUI()
 
 const proprietes = {
 	jumpHeight: 500,
@@ -21,38 +11,42 @@ const proprietes = {
 	appareil: 'clavier',
 	modeJeu: 'fps'
 }
+/**
+ * GUI
+ */
+let guiVisible
 
-gui.add(proprietes, 'jumpHeight', 0, 500).onChange((value) => {
-	proprietes.jumpHeight = value;
-});
-gui.add(proprietes, 'vitesse', 0, 1000).onChange((value) => {
-	proprietes.vitesse = value;
-});
-gui.add(proprietes, 'modeJeu', ['fps', '2d', 'vr']).onChange((value) => {
+if (import.meta.env.DEV) {
+	guiVisible = true;
+}
+else {
+	guiVisible = false;
+}
+
+
+const gui = new GUI();
+gui.add(proprietes, 'modeJeu', ['fps']).onChange((value) => {
 	proprietes.modeJeu = value;
 });
 
 
+document.addEventListener('keydown', (event) => {
+	if (event.key === '²') {
+		guiVisible = !guiVisible;
+		gui.domElement.style.display = guiVisible ? 'block' : 'none';
+	}
+});
 
 
-let camera, scene, renderer, controls;
 
+
+
+
+let camera, scene, renderer;
 const objects = [];
 
 let raycaster;
 
-let moveForward = false;
-let moveBackward = false;
-let moveLeft = false;
-let moveRight = false;
-let canJump = false;
-
-let prevTime = performance.now();
-const velocity = new THREE.Vector3();
-const direction = new THREE.Vector3();
-
-// PlayerControls
-const playerControls = new PlayerControls(proprietes.modeJeu);
 init();
 animate();
 
@@ -63,99 +57,15 @@ function init() {
 
 	scene = new THREE.Scene();
 	scene.background = new THREE.Color(0xffffff);
-	scene.fog = new THREE.Fog(0xffffff, 0, 750);
 
-
-	controls = new PointerLockControls(camera, document.body);
-
-	const blocker = document.getElementById('blocker');
-	const instructions = document.getElementById('instructions');
-
-	instructions.addEventListener('click', function () {
-
-		controls.lock();
-
-	});
-
-	controls.addEventListener('lock', function () {
-
-		instructions.style.display = 'none';
-		blocker.style.display = 'none';
-
-	});
-
-	controls.addEventListener('unlock', function () {
-		blocker.style.display = 'block';
-		instructions.style.display = '';
-
-	});
-
-	scene.add(controls.getObject());
-
-	const onKeyDown = function (event) {
-
-		switch (event.code) {
-			case 'ArrowUp':
-			case 'KeyW':
-				moveForward = true;
-				break;
-
-			case 'ArrowLeft':
-			case 'KeyA':
-				moveLeft = true;
-				break;
-
-			case 'ArrowDown':
-			case 'KeyS':
-				moveBackward = true;
-				break;
-
-			case 'ArrowRight':
-			case 'KeyD':
-				moveRight = true;
-				break;
-
-			case 'Space':
-				if (canJump === true) velocity.y += proprietes.jumpHeight;
-				canJump = false;
-				break;
-
-		}
-
-	};
-
-	const onKeyUp = function (event) {
-
-		switch (event.code) {
-
-			case 'ArrowUp':
-			case 'KeyW':
-				moveForward = false;
-				break;
-
-			case 'ArrowLeft':
-			case 'KeyA':
-				moveLeft = false;
-				break;
-
-			case 'ArrowDown':
-			case 'KeyS':
-				moveBackward = false;
-				break;
-
-			case 'ArrowRight':
-			case 'KeyD':
-				moveRight = false;
-				break;
-
-		}
-
-	};
-
-	document.addEventListener('keydown', onKeyDown);
-	document.addEventListener('keyup', onKeyUp);
 
 	raycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, - 1, 0), 0, 10);
+
+
+	// PlayerControls
+
+	const playerControls = new PlayerControls(proprietes.modeJeu, camera, raycaster, proprietes);
+	scene.add(playerControls.playerMove.controls.getObject())
 
 	// floor
 
@@ -212,7 +122,6 @@ function init() {
 						positionX = 300 + (compteurMusique * 200);
 						positionZ = -200;
 						compteurMusique++;
-						console.log(compteurMusique, element.type, positionX, positionZ)
 						break;
 					case 'website':
 						positionX = -300 - (compteurSite * 200);
@@ -225,9 +134,6 @@ function init() {
 						compteurVideo++;
 						break;
 				}
-				
-				console.log(positionX, positionZ,element.type)
-
 
 				// Ajoute le titre du projet
 				const titre = new Text3D(element.title, 12);
@@ -240,7 +146,7 @@ function init() {
 					const height = img.height;
 					const width = img.width;
 					const ratio = width / height;
-		
+
 					const jaquette = new THREE.TextureLoader().load(`/images/projects/logos/${element.imageName}`);
 					const jaquetteMaterial = new THREE.SpriteMaterial({ map: jaquette });
 					const jaquetteMesh = new THREE.Sprite(jaquetteMaterial);
@@ -280,59 +186,7 @@ function onWindowResize() {
 }
 
 function animate() {
-
+	
 	requestAnimationFrame(animate);
-
-	const time = performance.now();
-
-	if (controls.isLocked === true) {
-
-		raycaster.ray.origin.copy(controls.getObject().position);
-		raycaster.ray.origin.y -= 10;
-
-		const intersections = raycaster.intersectObjects(objects, false);
-
-		const onObject = intersections.length > 0;
-
-		const delta = (time - prevTime) / 1000;
-
-		velocity.x -= velocity.x * 10.0 * delta;
-		velocity.z -= velocity.z * 10.0 * delta;
-
-		velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
-
-		direction.z = Number(moveForward) - Number(moveBackward);
-		direction.x = Number(moveRight) - Number(moveLeft);
-		direction.normalize(); // this ensures consistent movements in all directions
-
-
-		if (moveForward || moveBackward) velocity.z -= direction.z * proprietes.vitesse * delta;
-		if (moveLeft || moveRight) velocity.x -= direction.x * proprietes.vitesse * delta;
-
-		if (onObject === true) {
-
-			velocity.y = Math.max(0, velocity.y);
-			canJump = true;
-
-		}
-
-		controls.moveRight(- velocity.x * delta);
-		controls.moveForward(- velocity.z * delta);
-
-		controls.getObject().position.y += (velocity.y * delta); // new behavior
-
-		if (controls.getObject().position.y < 20) {
-
-			velocity.y = 0;
-			controls.getObject().position.y = 20;
-
-			canJump = true;
-		}
-
-	}
-
-	prevTime = time;
-
 	renderer.render(scene, camera);
-
 }
--- assets/three/global/fps/movementFPS.js
+++ assets/three/global/fps/movementFPS.js
@@ -1,7 +1,88 @@
-import '../mouvement.js'
+import PlayerMove from '../mouvement.js';
+import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
 
-export default class PlayerFPS {
-    constructor () {
-        this.controls = new PointerLockControls(camera, document.body);
+export default class PlayerFPS extends PlayerMove {
+    constructor(camera, raycaster, proprietes) {
+        super(camera, raycaster, proprietes);
+        this.controls = new PointerLockControls(this.camera, document.body);
+
+        this.blocker = document.getElementById('blocker');
+        this.instructions = document.getElementById('instructions');
+
+        this.instructions.addEventListener('click', () => {
+
+            this.controls.lock();
+
+        });
+
+        this.controls.addEventListener('lock', () => {
+
+            this.instructions.style.display = 'none';
+            this.blocker.style.display = 'none';
+
+        });
+
+        this.controls.addEventListener('unlock', () => {
+            this.blocker.style.display = 'block';
+            this.instructions.style.display = '';
+
+        });
+
+        this.prevTime = performance.now();
+
+        this.update();
+    }
+
+
+
+    update() {
+        const time = performance.now();
+        if (this.controls.isLocked) {
+            this.raycaster.ray.origin.copy(this.controls.getObject().position);
+            this.raycaster.ray.origin.y -= 10;
+
+            //const intersections = this.raycaster.intersectObjects(objects, false);
+
+            //const onObject = intersections.length > 0;
+
+            const delta = (time - this.prevTime) / 1000;
+
+            this.velocity.x -= this.velocity.x * 10.0 * delta;
+            this.velocity.z -= this.velocity.z * 10.0 * delta;
+
+            this.velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+            this.direction.z = Number(this.up) - Number(this.down);
+            this.direction.x = Number(this.right) - Number(this.left);
+            this.direction.normalize(); // this ensures consistent movements in all directions
+
+
+            if (this.up || this.down) this.velocity.z -= this.direction.z * this.proprietes.vitesse * delta;
+            if (this.left || this.right) this.velocity.x -= this.direction.x * this.proprietes.vitesse * delta;
+
+            /*if (onObject === true) {
+
+                this.velocity.y = Math.max(0, this.velocity.y);
+                this. = true;
+
+            }*/
+
+            this.controls.moveRight(- this.velocity.x * delta);
+            this.controls.moveForward(- this.velocity.z * delta);
+
+            this.controls.getObject().position.y += (this.velocity.y * delta); // new behavior
+
+            if (this.controls.getObject().position.y < 20) {
+
+                this.velocity.y = 0;
+                this.controls.getObject().position.y = 20;
+
+                this.jump = true;
+            }
+        }
+
+        this.prevTime = time;
+
+        requestAnimationFrame(() => this.update());
     }
 }
